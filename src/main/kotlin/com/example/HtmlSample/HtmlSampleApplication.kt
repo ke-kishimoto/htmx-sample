6707dac6b060f4435d52c29343ac170df146248a
k.kishimoto <sys-blog@techno-core.jp>
2025-11-16 22:34:40 +0900

    first commit

---
+++ src/main/kotlin/com/example/HtmlSample/HtmlSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.HtmlSample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HtmlSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<HtmlSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/HtmlSample/HtmlSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.HtmlSample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HtmlSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
