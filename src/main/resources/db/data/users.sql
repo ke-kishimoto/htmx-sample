7c6b7207da974f22cc52be6afc09137f36f6b1bc
k.kishimoto <nebinosuk@gmail.com>
2025-11-16 23:39:52 +0900

    add to create sample data SQL

--- src/main/resources/db/data/users.sql
+++ src/main/resources/db/data/users.sql
@@ -3,3 +3,14 @@ values
 ('Alice', 'Alice@example.com'),
 ('Bob', 'Bob@example.com'),
 ('Chris', 'Chris@example.com');
+
+WITH RECURSIVE cnt AS (
+    SELECT 1 AS n
+    UNION ALL
+    SELECT n + 1 FROM cnt WHERE n < 10000
+)
+INSERT INTO users (name, email)
+SELECT
+    CONCAT('User', n) AS name,
+    CONCAT('user', n, '@example.com') AS email
+FROM cnt;
