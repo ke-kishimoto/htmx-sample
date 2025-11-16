c19256caebf9c8a504c141973b376c7133de8f9d
nebinosuk@gmail.com <sys-blog@techno-core.jp>
2025-11-16 22:49:36 +0900

    add index

--- src/main/kotlin/com/example/htmx/user/UserController.kt
+++ src/main/kotlin/com/example/htmx/user/UserController.kt
@@ -1,7 +1,13 @@
 package com.example.htmx.user
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class UserController {
+
+    @GetMapping("/")
+    fun index(): String {
+        return "list"
+    }
 }
